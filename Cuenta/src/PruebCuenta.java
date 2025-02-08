4b311b99c86e1d30598b371a8f3476df3f006784
demambogirl <pia@demambo.com>
2025-02-08 15:13:02 -0300

    Saldo de Cuenta

---
+++ Cuenta/src/PruebCuenta.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class PruebCuenta
+{
+    public static void main( String[] args )
+    {
+        Cuenta cuenta1 = new Cuenta(50.00);
+        Cuenta cuenta2 = new Cuenta(-7.53);
+
+        System.out.printf( "Saldo de cuenta1: $%.2f\n", cuenta1.obtenerSaldo());
+        System.out.printf( "Saldo de cuenta2: $%.2f\n\n", cuenta2.obtenerSaldo());
+
+        Scanner entrada = new Scanner(System.in);
+        double montoDeposito;
+        System.out.print( "Escriba el monto a depositar para cuenta1:");
+        montoDeposito = entrada.nextDouble();
+        System.out.printf( "\nsumando %.2f al saldo de cuenta1\n\n", montoDeposito);
+        cuenta1.abonar(montoDeposito);
+
+        System.out.printf( "Saldo de cuenta1: $%.2f\n", cuenta1.obtenerSaldo());
+        System.out.printf( "Saldo de cuenta2: $%.2f\n\n", cuenta2.obtenerSaldo());
+
+        System.out.print( "Escriba el monto a depositar para cuenta2: ");
+        montoDeposito = entrada.nextDouble();
+        System.out.printf( "\nsumando %.2f al saldo de cuenta2\n\n", montoDeposito);
+        cuenta2.abonar(montoDeposito);
+
+        System.out.printf( "Saldo de cuenta1: $%.2f\n", cuenta1.obtenerSaldo());
+        System.out.printf( "Saldo de cuenta2: $%.2f\n\n", cuenta2.obtenerSaldo());
+    }
+}
