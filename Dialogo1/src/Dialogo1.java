74b8653849f1cc85a9177ec7fa6e2dd4780ed4fb
demambogirl <pia@demambo.com>
2025-02-08 15:48:57 -0300

    Eliminar carpeta 'nombre-de-la-carpeta'

--- Dialogo1/src/Dialogo1.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class Dialogo1
-{
-    public static void main( String[] args )
-    {
-        JOptionPane.showMessageDialog(null, "Bienvenido\na\nJava");
-    }
-}
