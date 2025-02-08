2cb41dd2a6c03ec80863270c912f682caac2dfee
demambogirl <pia@demambo.com>
2025-02-08 15:51:42 -0300

    Diálogo N°1

---
+++ Dialogo1/src/DialogoNombre.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class DialogoNombre
+{
+    public static void main ( String[] args )
+    {
+        String nombre =
+                JOptionPane.showInputDialog("¿Cual es tu nombre?");
+
+        String mensaje =
+                String.format( "Bienvenido, %s, a la Programación en Java!", nombre);
+
+        JOptionPane.showMessageDialog( null, mensaje);
+        
+    }
+}
