9660897083633a0fc3f2f17a88a0bbac1fde4614
demambogirl <pia@demambo.com>
2025-02-09 11:19:38 -0300

    Arreglo incremento

---
+++ InicArreglo3/src/InicArreglo3.java
@@ -0,0 +1,24 @@
+public class InicArreglo3
+{
+    public static void main(String[] args)
+    {
+        if(args.length!= 3)
+            System.out.println("Error: Vuelva a escribir el comando completo, incluyendo\n" + "el tamanio del arreglo, el valor inicial y el incremento");
+        else
+        {
+            int longitudArreglo = Integer.parseInt(args[0]);
+            int[] arreglo = new int[longitudArreglo];
+
+            int valorInicial = Integer.parseInt(args[1]);
+            int incremento = Integer.parseInt(args[2]);
+
+            for(int contador = 0; contador < arreglo.length; contador++)
+                arreglo[contador] = valorInicial + incremento * contador;
+
+            System.out.printf("%s%8s\n", "Indice", "Valor");
+
+            for (int contador = 0; contador < arreglo.length; contador++)
+                System.out.printf("%5d%8d\n", contador, arreglo[contador]);
+        }
+    }
+}
